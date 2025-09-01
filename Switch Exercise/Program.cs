aa289d0480853ec3b30a0e99bb4a0b4619d3139f
joelb <joelb@DESKTOP-P8KJ30R>
2025-09-01 11:08:09 +0200

    Add project files.

---
+++ Switch Exercise/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Switch_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            char grade = 'C';
+
+            switch (grade)
+            {
+
+                case 'A':
+                    Console.WriteLine("Excellent!");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("Good Job!");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("You passed.");
+                    break;
+
+                default:
+                    Console.WriteLine("Better luck next time");
+                    break;
+
+            }
+            */
+
+
+            /*
+            int favoriteNumber = 14;
+
+            switch (favoriteNumber)
+            {
+
+                case 1:
+                    Console.WriteLine("You are number 1!");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Lucky number 7!");
+                    break;
+
+                case 10:
+                    Console.WriteLine("Perfect 10!");
+                    break;
+
+                default:
+                    Console.WriteLine("That's a nice number!");
+                    break;
+
+            }
+            */
+
+
+            /*
+            string day = "Saturday";
+
+            switch (day)
+
+            {
+
+                case "Monday":
+                    Console.WriteLine("Start of the week.");
+                    break;
+
+                case "Friday":
+                    Console.WriteLine("Almost weekend!");
+                    break;
+
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine("Weekend vibes!");
+                    break;
+
+                default:
+                    Console.WriteLine("Just another day.");
+                    break;
+
+            }
+            */
+
+
+            /*
+            char currencySymbol = '£';
+
+            switch (currencySymbol)
+            {
+
+                case '€':
+                    Console.WriteLine("You chose Euro.");
+                    break;
+
+                case '$':
+                    Console.WriteLine("You chose Dollar.");
+                    break;
+
+                case '£':
+                    Console.WriteLine("You chose Pound.");
+                    break;
+
+            }
+            */
+
+
+
+
+        }
+
+    }
+}
